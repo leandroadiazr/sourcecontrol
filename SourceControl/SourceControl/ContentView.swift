16436bf006fe61bf85742ce17017bcf50d147b13
Leandro Diaz <leandroadiazr@gmail.com>
2021-09-27 18:00:26 -0400

    testing a commit

---
+++ SourceControl/SourceControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControl
+//
+//  Created by Leandro Diaz on 9/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControl/SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Leandro Diaz on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
